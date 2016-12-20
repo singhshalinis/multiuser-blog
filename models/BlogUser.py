6ad006657604c25fbe2353515efc3516ad4d299e
singhshalinis <thisdatacloud@gmail.com>
2016-12-20 15:45:42 -0800

    Updated

--- models/BlogUser.py
+++ models/BlogUser.py
@@ -7,18 +7,13 @@ class BlogUser(db.Model):
     created = db.DateTimeProperty(auto_now_add=True)
     modified = db.DateTimeProperty(auto_now=True)
 
-    # @classmethod
-    # def by_id(cls, uid):
-    #     return BlogUser.get_by_id(uid, parent=users_key())
+    @classmethod
+    def by_id(cls, uid):
+        return BlogUser.get_by_id(uid)
 
-    # @classmethod
-    # def by_name(cls, name):
-    #     return BlogUser.all.filter('name=' + name).get()
-
-    # @classmethod
-    # def register(cls, name, pw, email):
-    #     pw_hash = make_secure_val(name, pw)
-    #     return BlogUser(parent=users_key(),
-    #                     username=name,
-    #                 password=pw_hash,
-    #                 email=email)
+    @classmethod
+    def validateUserid(userid):
+        if userid:
+            user = BlogUser.by_id(cookie_userid)
+            if user:
+                return user
