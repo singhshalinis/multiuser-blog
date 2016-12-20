29fc113d45f654960a0fa853528b533095298d48
singhshalinis <thisdatacloud@gmail.com>
2016-12-20 15:47:06 -0800

    Changes

--- models/Post.py
+++ models/Post.py
@@ -3,7 +3,7 @@ from BlogUser import BlogUser
 
 class Post(db.Model):
     writer = db.ReferenceProperty(BlogUser)
-    title = db.StringProperty(required=True)  # note the diff: title vs subject
+    title = db.StringProperty(required=True)  # note the diff: title & subject
     content = db.TextProperty(required=True)
     likes = db.IntegerProperty(default=0)
     created = db.DateTimeProperty(auto_now_add=True)
