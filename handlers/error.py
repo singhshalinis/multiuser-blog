f6d05c8d5378b1f1396f570f916d7369cac30558
singhshalinis <thisdatacloud@gmail.com>
2016-12-21 00:48:00 -0800

    Error Codes

--- handlers/error.py
+++ handlers/error.py
@@ -1,13 +1,15 @@
 from handler import Handler
 
 
-class Error_Codes:
-    POST_DOES_NOT_EXIST = 1
-    NOT_OWNER = 2
-
 
 # TO-DO: Error codes are not being used yet.
 # Implement the error codes (is it needed?)
+error_codes = {}
+error_codes{'POST_DOES_NOT_EXIST'} = "The post does not exist"
+error_codes{'NOT_OWNER'} = "Is not the owner"
+error_codes{'NOT_SIGNED_IN'} = "User is not signed in"
+
+
 class ErrorHandler(Handler):
     def render_front(self, error=""):
         self.render("error.html", error=error)
